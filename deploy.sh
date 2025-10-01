d6d5aa53bfdf5c28d0dc6937809584c82ca5d753
ayoajetunmobi <67721103+ayoajetunmobi@users.noreply.github.com>
2025-10-01 02:21:16 +0100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd /home/ajeh/nodeapp
+git clone https://github.com/ayoajetunmobi/nextjs_jenkins.git
+cd /home/ajeh/nodeapp/nextjs_jenkins
+npm install
+npm run build
+sudo -S systemctl restart reactapp</home/ajeh/file
